32bf18dff0dcc708ce03aec409f523f3a7a13d37
prydt <prydt@waifu.club>
2017-09-17 17:55:38 -0500

    will display images now =_=

---
+++ scripts/assets.js
@@ -0,0 +1,12 @@
+let assets = ['grayrough.png', 'yellowrough.png']
+let imgs = []
+
+for(let i = 0; i < assets.length; i++) {
+  let temp = new Image()
+  temp.src = './assets/' + assets[i]
+  
+  imgs.push(temp)
+}
+
+exports.assetList = assets
+exports.imgs = imgs
--- scripts/editor.js
+++ scripts/editor.js
@@ -1,5 +1,32 @@
-let canvas = document.getElementById('cnv');
+// context for drawing to the canvas
+let canvas = document.getElementById('cnv')
 let ctx = canvas.getContext('2d')
 
-ctx.fillStyle = '#000'
-ctx.fillRect(0, 0, 800, 600)
+// each tile is 16 by 16, but they are rendered as 48 by 48
+const blocksize = 48
+
+// gets asset files from assets.js
+const imgs = require('./scripts/assets.js').imgs
+
+// stores map and default map size
+let map = []
+let width = 10, height = 10
+
+for (var i = 0; i < height; i++) {
+	map.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+}
+
+function draw() {
+	drawMap()
+	requestAnimationFrame(draw)
+}
+
+function drawMap() {
+	for (var i = 0; i < map.length; i++) {
+		for (var j = 0; j < map[i].length; j++) {
+      ctx.drawImage(imgs[map[i][j]], i * blocksize, j * blocksize, blocksize, blocksize)
+		}
+	}
+}
+
+requestAnimationFrame(draw)
